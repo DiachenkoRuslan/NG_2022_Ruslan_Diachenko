45ff20cf400ca8f9ee6284cd2cbfd99ca7a9327a
NoneGOSs <diachenkoruslanofficial@gmail.com>
2022-10-18 15:46:52 +0300

    Added complete task2.py

--- Lesson_1/task2.py
+++ Lesson_1/task2.py
@@ -1,1 +1,41 @@
 
+#Important notice!
+print("NOTICE! ^ - a squared b.(Example: 2^3 = 8).")
+print("NOTICE! ~ - root a on b. (Example: 16~2 = 4).")
+# Enter first number 
+firstNumber = int(input("Enter first number: "))
+
+# Enter second number 
+secondNumber = int(input("Enter second number: "))
+
+# Enter operation(+, -, *, /) for firstNumber and secondNumber
+operation = input("Enter operation( +, - , *, /, ^, ~ ): ")
+
+
+# if operation +, output: a + b = c
+if operation == '+':
+    print("Result: " + str(firstNumber) + operation + str(secondNumber) + " = " + str(firstNumber + secondNumber))
+
+# if operation -, output: a - b = c
+elif operation == "-":
+    print("Result: " + str(firstNumber) + operation + str(secondNumber) + " = " + str(firstNumber - secondNumber))
+
+# if operation *, output: a * b = c
+elif operation == "*":
+    print("Result: " + str(firstNumber) + operation + str(secondNumber) + " = " + str(firstNumber * secondNumber))
+
+# if operation /, output: a / b = c
+elif operation == "/":
+    print("Result: " + str(firstNumber) + operation + str(secondNumber) + " = " + str(firstNumber / secondNumber))
+
+# if operation *, output: a ^ b = c
+elif operation == "^":
+    print("Result: " + str(firstNumber) + operation + str(secondNumber) + " = " + str(firstNumber ** secondNumber))
+
+# if operation ~, output: a ~ b = c (root operation)
+elif operation == "~":
+    print("Result: " + str(firstNumber) + operation + str(secondNumber) + " = " + str(firstNumber ** (1/secondNumber)))
+
+# if operation not equal +, -, *, /
+else:
+    print("I don't know [ " + operation + " ] operation!")
