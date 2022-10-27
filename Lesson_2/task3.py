fcedafb3b94fae2fff9d1cefff612158345eaf4d
NoneGOSs <diachenkoruslanofficial@gmail.com>
2022-10-27 21:39:15 +0300

    Added edit task3!

--- Lesson_2/task3.py
+++ Lesson_2/task3.py
@@ -1,12 +1,12 @@
-number = int(input("Enter number: "))
+number = int(input("Enter number: ")) # Enter number
 
 numList = []
-for i in range(number):
-    numList.append(number)
-    number-=1
+for i in range(number):             # Start itteration(i) in range by length number,  
+    numList.append(number)          # add number to numMassive,
+    number-=1                       # number - 1 -> delete added number.
 
-for i in range(len(numList)):
-    for j in numList:
-        print(j, end=" ")
-    print("")
-    del numList[0]
+for i in range(len(numList)):       # Start itteration(i) in range length numList,
+    for num in numList:             #  Number in numList,
+        print(num, end=" ")         #  output without "newline"
+    print("")                       #  space between numbers
+    del numList[0]                  #  delete FIRST element in numList
