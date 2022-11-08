0498739fb5f58d2081780d43fca8eb4a04ca46de
NoneGOSs <diachenkoruslanofficial@gmail.com>
2022-11-08 20:29:35 +0200

    Edit task2.py. I edit this task by easy way, before solution has been bad.

--- Lesson_2/task2.py
+++ Lesson_2/task2.py
@@ -1,13 +1,3 @@
-inputList  = input("Enter elements of list: ").split(",")
+inputText = set(input().replace(" ", "").split(","))
 
-for index in range(len(inputList)):                 # Start index with 0 to range length inputList,
-    inputList[index] = inputList[index].strip()     # Remove spaces in start and end
-
-noRepeatList = {""}
-
-for i in range(len(inputList)):             # Start itterations(i) in length by lengthList,
-    noRepeatList.add(str(inputList[i]))     # Add to noRepeatList element from inputList by index,
-
-noRepeatList.discard("")                    # Delete "" from noRepeatList because without it element(or other 1 element),
-                                            # Method .add() not work!
-print(noRepeatList)
+print("Non repeate list: ", inputText)
