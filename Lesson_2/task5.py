edd20074317b2d874849a76ab057bafc04ab0f96
NoneGOSs <diachenkoruslanofficial@gmail.com>
2022-11-11 12:13:13 +0200

    Edit task5 without cycle!

--- Lesson_2/task5.py
+++ Lesson_2/task5.py
@@ -1,14 +1,7 @@
-inputList  = input("Enter elements of list: ").split(",")
-
-numList = []
-sumNumList = 0
-
-for elem in inputList:
-    numList.append(int(elem))
-for elem in numList:
-    sumNumList += elem
+inputList  = list(map(int, input("Enter elements of list: ").split(",")))
+print(inputList)
 
 
-print("Smallest number in list: ", min(numList))
-print("Biggest number in list: ", max(numList))
-print("Sum all numbers in list: ", sumNumList)
+print("Smallest number in list: ", min(inputList))
+print("Biggest number in list: ", max(inputList))
+print("Sum all numbers in list: ", sum(inputList))
