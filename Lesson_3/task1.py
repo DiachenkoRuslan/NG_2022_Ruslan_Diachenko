c5f6ca4c1c3153a6d45a7dc67f37c05549b45e4a
NoneGOSs <diachenkoruslanofficial@gmail.com>
2022-11-11 12:20:18 +0200

    Delete useless op in functions parameters

--- Lesson_3/task1.py
+++ Lesson_3/task1.py
@@ -4,37 +4,37 @@ def inputNum(text):
 def inputOp():
     return input("Enter operation (+, -, *, /, ^, ~): ")
 
-def addNums(num1, op, num2):
+def addNums(num1,num2):
     return num1 + num2
 
-def minusNums(num1, op, num2):
+def minusNums(num1,num2):
     return num1 - num2
 
-def divNums(num1, op, num2):
+def divNums(num1,num2):
     return num1 / num2
 
-def multipleNums(num1, op, num2):
+def multipleNums(num1,num2):
     return num1 * num2
 
-def powerNums(num1, op, num2):
+def powerNums(num1, num2):
     return num1 ** num2
 
-def squareNums(num1, op, num2):
+def squareNums(num1, num2):
     return num1 ** (1/num2)
 
 def result(num1, op, num2):
     if op == "+":
-        return addNums(num1, op, num2)
+        return addNums(num1, num2)
     if op == "-":
-        return minusNums(num1, op, num2)
+        return minusNums(num1, num2)
     if op == "*":
-        return divNums(num1, op, num2)
+        return divNums(num1, num2)
     if op == "/":
-        return multipleNums(num1, op, num2)
+        return multipleNums(num1, num2)
     if op == "^":
-        return powerNums(num1, op, num2)
+        return powerNums(num1, num2)
     if op == "~":
-        return squareNums(num1, op, num2)
+        return squareNums(num1, num2)
 
 def main():
     num1 = inputNum("Enter num1: ")
